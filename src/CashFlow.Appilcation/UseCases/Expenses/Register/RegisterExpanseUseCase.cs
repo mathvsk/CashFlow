529db1134fae4023ad35dbe7ed004336a03b5217
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-04-24 07:12:38 -0300

    feat: create initial function to validate data received by the endpoint.

--- src/CashFlow.Appilcation/UseCases/Expenses/Register/RegisterExpanseUseCase.cs
+++ src/CashFlow.Appilcation/UseCases/Expenses/Register/RegisterExpanseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Appilcation.UseCases.Expenses.Register;
@@ -7,6 +8,33 @@ public class RegisterExpanseUseCase
 {
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validate(request);
         return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("Title is required");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("Value must be greater than zero");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0)
+        {
+            throw new ArgumentException("Date cannot be greater than the current date");
+        }
+
+        var paymentTypeIsValida = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (!paymentTypeIsValida)
+        {
+            throw new ArgumentException("Invalid payment type");
+        }
     }
 }
