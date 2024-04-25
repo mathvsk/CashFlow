30e782007ffb99c0d9b7538e534013f22e900bc2
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-04-25 20:31:49 -0300

    feat: Create a general error filter for the application - thus avoiding the need to use try-catch in every request.

---
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,41 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is CashFlowException) {
+            HandleProjectException(context);
+        } else
+        {
+            ThrowUnknowError(context);
+        } 
+
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            var ex = context.Exception as ErrorOnValidationException;
+
+            var errorsResponse = new ResponseErrorJson(ex!.Erros);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new ObjectResult(errorsResponse);
+        }
+    }
+
+    private void ThrowUnknowError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson("Unknow error");
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+}
