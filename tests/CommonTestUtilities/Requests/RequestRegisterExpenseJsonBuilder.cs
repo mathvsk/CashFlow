991552728d9427880f22c6d9a9ac3f6565e0a363
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-05-06 18:26:02 -0300

    feat: create fictitious data for unit tests. - use NuGet package Bogus.

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public static RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(x => x.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(x => x.Date, faker => faker.Date.Past())
+            .RuleFor(x => x.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(x => x.Amount, faker => faker.Random.Decimal(min: 1, max: 1000))
+            .RuleFor(x => x.PaymentType, faker => faker.PickRandom<CashFlow.Communication.Enums.PaymentType>());
+    }
+}
