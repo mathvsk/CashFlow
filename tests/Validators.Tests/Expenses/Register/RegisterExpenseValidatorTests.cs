23f3b784f973a4657e3e64119f794362a265a7be
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-05-06 18:08:49 -0300

    feat: create test outline.

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,29 @@
+﻿using CashFlow.Appilcation.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new RegisterExpanseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Title",
+            Date = DateTime.UtcNow.AddDays(-1),
+            Description = "Description",
+            Amount = 1,
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CREDIT_CARD
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+
+    }
+}
