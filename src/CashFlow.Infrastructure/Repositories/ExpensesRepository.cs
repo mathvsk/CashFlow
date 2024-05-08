46ab0d3537b6c1df4623562c6de9c55a8a20a610
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-05-08 18:00:39 -0300

    feat: create a repository to persist an expense entity using Entity Framework.

---
+++ src/CashFlow.Infrastructure/Repositories/ExpensesRepository.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
+
+namespace CashFlow.Infrastructure.Repositories;
+
+internal class ExpensesRepository : IExpensesRepository
+{
+    public void Add(Expense expense)
+    {
+        var dbContext = new CashFlowDbContext();
+
+        dbContext.Expenses.Add(expense);
+        dbContext.SaveChanges();
+    }
+}
