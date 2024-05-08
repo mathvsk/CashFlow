47315ba559957c9c87703659c16f574e0a56c18c
Matheus Igor <matheus.igor.01tec@gmail.com>
2024-05-08 08:32:39 -0300

    feat: create connection to database.

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=root;";
+        var MySqlVersion = new Version(8, 0, 37);
+        var serverVersion = new MySqlServerVersion(MySqlVersion);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
